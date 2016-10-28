d4aedd2d1c204719632d8c6f70006ccb8f6cfdae
levon <levont@energizeglobal.com>
2016-10-28 18:58:06 +0400

    refactoring

--- src/main/java/com/example/controller/AdminController.java
+++ src/main/java/com/example/controller/AdminController.java
@@ -6,8 +6,11 @@ import com.example.manager.UserManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 
 /**
@@ -27,6 +30,12 @@ public class AdminController {
         } catch (DatabaseException e) {
             e.printStackTrace();
         }
+        return "admin/home";
+    }
+
+    @RequestMapping(value = "/delete/{0}", method = RequestMethod.GET)
+    public String delete(@PathParam("id") Long id) throws DatabaseException {
+        userManager.delete(id);
         return "admin/home";
     }
 
